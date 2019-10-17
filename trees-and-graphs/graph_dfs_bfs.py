a92a84bdaa29ab8a460c3f1f5ed5fb92e9d704ea
vedant.pandey <vedant.pandey@obopay.com>
2019-10-17 18:10:28 +0530

    converted to modules

--- trees-and-graphs/graph_dfs_bfs.py
+++ trees-and-graphs/graph_dfs_bfs.py
@@ -6,7 +6,19 @@ class Graph :
     self.graph = ddict(list)
 
   def add_edge(self, u, v) :
-    self.graph[u].append[v]
+    self.graph[u] += [v]
+
+  def generate_edges(self) :
+    
+    edges = []
+
+    for node in self.graph :
+      for neighbour in self.graph[node] :
+        edges.append((node, neighbour))
+    
+    return edges
+
+class GraphWithSearch(Graph) :
 
   def DFS_util(self, v, visited) :
     visited[v] = True
@@ -17,12 +29,17 @@ class Graph :
         self.DFS_util(i, visited)
 
   def DFS(self, v) :
-    visited = [False] * (len(self.graph))
+    print()
 
+    visited = [False] * (len(self.graph))
     self.DFS_util(v, visited)
+
+    print()
 
   def BFS(self, s) :
     
+    print()
+
     visited = [False] * (len(self.graph))
     queue = []
     queue.append(s)
@@ -36,23 +53,25 @@ class Graph :
         if visited[i] == False:
           queue.append(i)
           visited[i] = True
+    
+    print()
 
 if __name__ == "__main__":
-  b_or_d = int(input('1 - BFS \n2 - DFS\n\n'))
-
+  b_or_d = int(input('1 - BFS \n2 - DFS\n'))
+  print()
   if b_or_d == 2 :
-    g1 = Graph()
+    g1 = GraphWithSearch()
     g1.add_edge(0, 1) 
     g1.add_edge(0, 2) 
     g1.add_edge(1, 2) 
     g1.add_edge(2, 0) 
-    g1.add_edge(2, 3) 
+    g1.add_edge(2, 3)
     g1.add_edge(3, 3) 
     print("Following is DFS from (starting from vertex 2)") 
     g1.DFS(2)
 
   else :
-    g2 = Graph() 
+    g2 = GraphWithSearch() 
     g2.add_edge(0, 1) 
     g2.add_edge(0, 2) 
     g2.add_edge(1, 2) 
---
+++ trees-and-graphs/minimal_tree.py
@@ -0,0 +1,1 @@
+from graph_dfs_bfs import Graph
